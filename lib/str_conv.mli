fdb4a9bef7d5f25e497361ca2788a65bafc07eaa
jpx40 <92295347+jpx40@users.noreply.github.com>
2024-09-20 23:57:20 +0200

    Update str_conv.mli

--- lib/str_conv.mli
+++ lib/str_conv.mli
@@ -17,7 +17,7 @@ end
 module type S = sig
   type t
   val of_substring : string -> pos:int -> (t * int)
-  val of_string : string -> t
+(*   val of_string : string -> t *)
   val to_string : t -> string
   val to_string_bin : t -> string
   val to_string_oct : t -> string
